e81e5f4049110d237030eab7a67362fa35c012a5
brylletejada <username927@go.byuh.edu>
2024-06-24 00:26:04 -1000

    Final Project Final

--- FinalProject/scripts/scripts.js
+++ FinalProject/scripts/scripts.js
@@ -10,5 +10,26 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("All fields are required!");
             event.preventDefault();
         }
+    
+        document.querySelector('form').addEventListener('submit', function(event) {
+            event.preventDefault();
+            const form = event.target;
+            fetch(form.action, {
+                method: form.method,
+                body: new FormData(form),
+                headers: {
+                    'Accept': 'application/json'
+                }
+            }).then(response => {
+                if (response.ok) {
+                    alert('Thank you for your feedback!');
+                    form.reset();
+                } else {
+                    alert('Oops! There was a problem submitting your form');
+                }
+            }).catch(error => {
+                alert('Oops! There was a problem submitting your form');
+            });
+        });
     });
 });
